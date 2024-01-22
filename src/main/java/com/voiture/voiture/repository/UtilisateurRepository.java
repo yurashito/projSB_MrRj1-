ceea4e90049ae618887f4bbf00e0784d0690d5e4
yurashito <118957930+yurashito@users.noreply.github.com>
2024-01-22 14:48:50 +0100

    modification utilisateurService et utilisateurRepository

--- src/main/java/com/voiture/voiture/repository/UtilisateurRepository.java
+++ src/main/java/com/voiture/voiture/repository/UtilisateurRepository.java
@@ -6,5 +6,8 @@ import com.voiture.voiture.modele.Utilisateur;
 
 
 public interface UtilisateurRepository extends   JpaRepository<Utilisateur , Integer>  {
+
+    @Query(value = "select * from utilisateur where :nom = nomUtilisateur and :mdp =  mdp " , nativeQuery = true)
+    Utilisateur verifierUtilisateur(@Param("nom") String nom , @Param("mdp") String mdp);
     
 }
