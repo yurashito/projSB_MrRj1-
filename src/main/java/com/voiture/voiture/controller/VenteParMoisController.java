c824601a5dd902e218b61ce645dff0c94bee4fab
DiaryRz <119029048+DiaryRz@users.noreply.github.com>
2024-01-28 15:11:04 +0100

    stat

--- src/main/java/com/voiture/voiture/controller/VenteParMoisController.java
+++ src/main/java/com/voiture/voiture/controller/VenteParMoisController.java
@@ -1,6 +1,7 @@
 package com.voiture.voiture.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/vente-par-mois")
+@CrossOrigin
 public class VenteParMoisController {
 
     private final VenteParMoisService venteParMoisService;
