60a7dda6f7159d4a721d83fdd60a7490f76d01ca
yurashito <118957930+yurashito@users.noreply.github.com>
2024-01-29 13:13:38 +0100

    modifaction UtilisateurRepository

--- src/main/java/com/voiture/voiture/repository/UtilisateurRepository.java
+++ src/main/java/com/voiture/voiture/repository/UtilisateurRepository.java
@@ -9,7 +9,7 @@ import com.voiture.voiture.modele.Utilisateur;
 
 public interface UtilisateurRepository extends   JpaRepository<Utilisateur , Integer>  {
 
-    @Query(value = "select * from utilisateur where :nom = nomUtilisateur and :mdp =  mdp " , nativeQuery = true)
-    Utilisateur verifierUtilisateur(@Param("nom") String nom , @Param("mdp") String mdp);
+    @Query(value = "select * from utilisateur where :mail = mail and :mdp =  mdp " , nativeQuery = true)
+    Utilisateur verifierUtilisateur(@Param("mail") String nom , @Param("mdp") String mdp);
     
 }
